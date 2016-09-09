bffb646ddfd40acc4bdf81747d978e4010399d20
Isaac Hess <isaachess@gmail.com>
2016-09-09 15:51:19 -0700

    Moving functions around

--- parallel.go
+++ parallel.go
@@ -10,26 +10,24 @@ import (
 	"time"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
+func get_num_routines(stop int) int {
 	min_routines := 1
 	max_routines := 1000
-	for {
-		text, _ := reader.ReadString('\n')
-		text2 := strings.Replace(text, "\n", "", -1)
-		num, _ := strconv.Atoi(text2)
-		num_routines := num / 10000
-		if num_routines < min_routines {
-			num_routines = min_routines
-		} else if num_routines > max_routines {
-			num_routines = max_routines
-		}
-		fmt.Println("num_routines", num_routines)
-		start := time.Now()
-		sum := sum_me(num, num_routines)
-		fmt.Println("time", time.Since(start))
-		fmt.Println("sum", sum)
+	num_routines := stop / 10000
+	if num_routines < min_routines {
+		return min_routines
+	} else if num_routines > max_routines {
+		return max_routines
 	}
+	return num_routines
+}
+
+func get_new_stop(start int, stop int, diff int) int {
+	new_stop := start + diff
+	if new_stop > stop {
+		new_stop = stop
+	}
+	return new_stop
 }
 
 func sum_me(stop int, num_routines int) int {
@@ -39,10 +37,7 @@ func sum_me(stop int, num_routines int) int {
 	ch := make(chan int)
 	start := 0
 	for i := 0; i < num_routines; i++ {
-		new_stop := start + int_diff
-		if new_stop > stop {
-			new_stop = stop
-		}
+		new_stop := get_new_stop(start, stop, int_diff)
 		go sum_parallel(start, new_stop, ch)
 		start = start + int_diff + 1
 	}
@@ -65,4 +60,19 @@ func sum_numbers(start int, stop int) int {
 		final += i
 	}
 	return final
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		text, _ := reader.ReadString('\n')
+		text2 := strings.Replace(text, "\n", "", -1)
+		num, _ := strconv.Atoi(text2)
+		num_routines := get_num_routines(num)
+		fmt.Println("num_routines", num_routines)
+		start := time.Now()
+		sum := sum_me(num, num_routines)
+		fmt.Println("time", time.Since(start))
+		fmt.Println("sum", sum)
+	}
 }
